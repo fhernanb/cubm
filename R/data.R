113f0d1a897df3d0807944aa4fc46549217d6310
fhernanb <fhernanb@unal.edu.co>
2017-09-12 14:40:28 -0500

    improved

--- R/data.R
+++ R/data.R
@@ -8,19 +8,19 @@
 #'
 #' @format A data frame with 53940 rows and 10 variables:
 #' \describe{
-#'   \item{Faculty}{A factor variable, with levels ranging from 1 to 13 indicating the coding for the different university faculties}
-#'   \item{Freqserv}{A factor with levels: 0 = for not regular users, 1 = for regular users}
-#'   \item{Age}{Variable indicating the age of the respondent in years}
+#'   \item{faculty}{A factor variable, with levels ranging from 1 to 13 indicating the coding for the different university faculties}
+#'   \item{freqserv}{A factor with levels: 0 = for not regular users, 1 = for regular users}
+#'   \item{age}{Variable indicating the age of the respondent in years}
 #'   \item{lage}{logarithm of age}
-#'   \item{Gender}{A factor with levels: 0 = man, 1 = woman}
-#'   \item{Diploma}{A factor with levels: 1 = classic studies, 2 = scientific studies, 3 = linguistic, 4 = Professional, 5 = Technical/Accountancy, 6 = others}
-#'   \item{Residence}{A factor with levels: 1 = city NA, 2 = district NA, 3 = others}
-#'   \item{ChangeFa}{A factor with levels: 1 = changed faculty, 2 = not changed faculty}
-#'   \item{Informat}{Level of satisfaction about the collected information}
-#'   \item{Willingn}{Level of satisfaction about the willingness of the staff}
-#'   \item{Officeho}{Judgment about the Office hours}
-#'   \item{Competen}{Judgement about the competence of the staff}
-#'   \item{Global}{Global satisfaction}
+#'   \item{gender}{A factor with levels: 0 = man, 1 = woman}
+#'   \item{diploma}{A factor with levels: 1 = classic studies, 2 = scientific studies, 3 = linguistic, 4 = Professional, 5 = Technical/Accountancy, 6 = others}
+#'   \item{residence}{A factor with levels: 1 = city NA, 2 = district NA, 3 = others}
+#'   \item{changeFa}{A factor with levels: 1 = changed faculty, 2 = not changed faculty}
+#'   \item{informat}{Level of satisfaction about the collected information}
+#'   \item{willingn}{Level of satisfaction about the willingness of the staff}
+#'   \item{officeho}{Judgment about the Office hours}
+#'   \item{compete}{Judgement about the competence of the staff}
+#'   \item{global}{Global satisfaction}
 #' }
 #' @source \url{https://cran.r-project.org/web/packages/CUB/CUB.pdf}
 "univer"
