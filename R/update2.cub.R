e74977919646f86110822988b4b4b5604be3def4
fhernanb <fhernanb@unal.edu.co>
2017-10-04 13:29:29 -0500

    the new update2 function

---
+++ R/update2.cub.R
@@ -0,0 +1,34 @@
+#' update2.cub
+#' 
+#' Update and Re-fit a Model Call
+#' 
+#' @param n number of observations
+#' @param pi uncertainty parameter belongs to \code{(0, 1]}
+#' @param xi feeling parameter belongs to \code{[0, 1]}
+#' @param m the maximum value for random values
+#' @param shift the minimum value for random values
+#' 
+#' @examples
+#' # The first model
+#' mod1 <- cub(pi.fo = global ~ 1, xi.fo = ~ 1, m=7, data=univer)
+#' # Modifying right sides of pi and xi formulas
+#' mod2 <- update2(mod1, pi.fo = global ~ gender, xi.fo = ~ lage + gender)
+#' # Modifying the dataset
+#' mod3 <- update2(xi.fo = ~ 1, m=7, data=univer[1:50, ])
+#'
+#' @export
+#' 
+update2.cub <- function(object, ..., evaluate=TRUE) {
+if (is.null(call <- getCall(object))) 
+  stop("need an object with call component")
+extras <- match.call(expand.dots = FALSE)$...
+if (length(extras)) {
+  existing <- !is.na(match(names(extras), names(call)))
+  for (a in names(extras)[existing]) call[[a]] <- extras[[a]]
+  if (any(!existing)) {
+    call <- c(as.list(call), extras[!existing])
+    call <- as.call(call)
+  }
+}
+call
+}
