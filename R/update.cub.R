063348ca361ebc07c9ce42b477424607ba404199
fhernanb <fhernanb@unal.edu.co>
2017-10-04 13:29:15 -0500

    we included some spaces

--- R/update.cub.R
+++ R/update.cub.R
@@ -17,15 +17,14 @@ update.cub <- function (object,
     else  match.arg(what)
   }
   
-  if (what=="pi") 
-  { call$pi.fo <- update.formula(formula(object, what),
-                                 formula.) }
+  if (what == "pi") 
+  { call$pi.fo <- update.formula(formula(object, what), formula.) }
   
-  else if (what=="xi")  
+  else if (what == "xi")  
   {
-    call[[paste(what,"fo",sep=".")]] <- 
-      if (length(update.formula(formula(object,what), formula.))==2)
-        update.formula(formula(object,what), formula.)
+    call[[paste(what, "fo", sep=".")]] <- 
+      if (length(update.formula(formula(object, what), formula.))==2)
+        update.formula(formula(object, what), formula.)
     else
       update.formula(formula(object, what), formula.)[-2]
   }
@@ -42,13 +41,13 @@ formula.cub <- function(x, what = c("pi", "xi"),
   what <- if (!is.null(parameter))  {
     match.arg(parameter, choices=c("pi", "xi"))} 
   else  match.arg(what)
-  if (!what%in%x$parameters) stop(paste(what,"is not a parameter in the object","\n")) 
+  if (!what%in%x$parameters) stop(paste(what, "is not a parameter in the object","\n")) 
   fo <- x[[paste(what, "fo", sep=".")]]
   ## the problem is when "." is in the formula, if true get formula from terms
   if (length(fo)==2 && "."%in%strsplit(as.character(fo),split="")[[2]])# no resp 
-    fo <- formula(x[[paste(what,"terms",sep=".")]])
+    fo <- formula(x[[paste(what, "terms", sep=".")]])
   if (length(fo)==3 &&  "."%in%strsplit(as.character(fo), split = "")[[3]])# "."%in%strsplit(as.character(fo), split = "")[[3]]
-    fo <- formula(x[[paste(what,"terms",sep=".")]])
+    fo <- formula(x[[paste(what, "terms", sep=".")]])
   fo
 }
 
